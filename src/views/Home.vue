19083719798de45cddced7515c2ba2ff05160fa8
shweyin <shweyin@gmail.com>
2020-03-01 19:47:00 -0500

    updated home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <v-img src="@/assets/background.jpg">
-    <div class="home">
-        <v-timeline dense>
-          <v-timeline-item>
-            Profile Pic
-          </v-timeline-item>
-          <v-timeline-item>
-            Intro
-          </v-timeline-item>
-          <v-timeline-item>
-            Resume
-          </v-timeline-item>
-        </v-timeline>    
-    </div>
+    <v-row>
+      <v-col cols="3">
+        <v-timeline dense>            
+          <v-hover v-slot:default="{hover}" close-delay="1000">
+            <v-timeline-item fill-dot color="blue-grey" icon="mdi-account">
+              <v-expand-transition>
+                <v-card v-if="hover">
+                  <v-card-title ><v-avatar class="mx-1"><v-img src="@/assets/profile.jpg"></v-img></v-avatar>Welcome to my Website!</v-card-title>
+                  <v-card-text>This is currently a work in progress. I work on this website in my free time.</v-card-text>            
+                </v-card>
+              </v-expand-transition>              
+            </v-timeline-item>
+          </v-hover>
+          <v-hover v-slot:default="{hover}" close-delay="1000">
+            <v-timeline-item fill-dot color="blue-grey" icon="mdi-card-text-outline">
+              <v-expand-transition>
+                <v-card v-if="hover">
+                  <v-card-text>My name is Shweyin. I'm taking an Honors Bachelors in Software Developing. This website is a hobby project that I'm working on to learn VueJS.</v-card-text>                  
+                </v-card>
+              </v-expand-transition>              
+            </v-timeline-item>
+          </v-hover>
+          <v-hover v-slot:default="{hover}" close-delay="1000">
+            <v-timeline-item fill-dot color="blue-grey" icon="mdi-script-text-outline">
+              <v-expand-transition>
+                <v-card v-if="hover">
+                  <v-card-text>I'm currently searching for Co-op opportunities for Summer 2020 and Winter 2020! Here's a link to my <a href="./assets/Shweyin_Than_Resume.pdf" download>resume</a>.</v-card-text>
+                  
+                </v-card>
+              </v-expand-transition>              
+            </v-timeline-item>
+          </v-hover>              
+        </v-timeline>
+      </v-col>
+    </v-row>
+    
   </v-img>  
 </template>
 
@@ -20,6 +43,10 @@
 
 export default {
   name: 'Home',
+  data() {
+    return {
+    }
+  },
   components: {
   }
 }
