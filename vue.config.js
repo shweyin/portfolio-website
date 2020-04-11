b6e15db98237f398ada60c74f1cd7108cb364b99
shweyin <shweyin@gmail.com>
2020-04-11 14:43:29 -0400

    fix file path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/~bth645_201b30/'
-    : '/',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/~bth645_201b30/'
+  //   : '/',
   "transpileDependencies": [
     "vuetify"
   ]
